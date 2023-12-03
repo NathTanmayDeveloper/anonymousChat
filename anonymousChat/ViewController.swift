1bfc601476fe25dae2d411dc80fae6cdde8d8217
Tanmay Chandra Nath <nathtanmaydeveloper@gmail.com>
2023-12-03 21:48:45 +0530

    chore: added UI

--- anonymousChat/ViewController.swift
+++ anonymousChat/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var joinTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func joinButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func createNewButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
